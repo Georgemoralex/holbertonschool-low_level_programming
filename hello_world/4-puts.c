6e48112f33d5600876ce5fa4e1a0b3a74d21e55f
Georgemoralex <morales.hotch@gmail.com>
2023-10-18 21:23:49 -0700

    c code

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * Function main
+ * 
+ * Print a message using puts function 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
