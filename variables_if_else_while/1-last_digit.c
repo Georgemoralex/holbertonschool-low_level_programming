ad48d3242e552fc3187d658bf1852314fcbe246b
Georgemoralex <morales.hotch@gmail.com>
2023-10-19 00:40:12 -0700

    last digit change in function name

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 	lastD = n % 10;
 	if (lastD > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, lastD);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastD);
 	}
 	else if (lastD < 6 && lastD != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6", n, lastD);
+		printf("Last digit of %d is %d and is less than 6\n", n, lastD);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0", n, lastD);
+		printf("Last digit of %d is %d and is 0\n", n, lastD);
 	}
 	return (0);
 }
