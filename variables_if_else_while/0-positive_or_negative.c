4f30f34b25af15c2dd9d67ac30002373c7042731
Georgemoralex <morales.hotch@gmail.com>
2023-10-18 23:48:28 -0700

    random number c code 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+*main - 0. Positive anything is better than negative nothing
+*Description function to evaluate random number as positive, negative or zero
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else
+	{
+		printf("%d is zero", n);
+	}	
+	return (0);
+}
