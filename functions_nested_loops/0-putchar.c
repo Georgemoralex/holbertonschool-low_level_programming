08ef8d04a3ad3b56855121e4bc3acb8cac6fd229
Georgemoralex <morales.hotch@gmail.com>
2023-10-19 19:49:34 -0700

    c functions nested loops

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - 0 putchar
+*Description: program to print _putchar
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char var = "_putchar";
+	
+	printf("%s", var);
+	putchar('\n');
+	return (0);
+}
---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - 2 print alphabet
+*Description: program to print alphabet
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(48 + i);
+		if (i != 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		else
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
