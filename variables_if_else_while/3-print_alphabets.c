1c1c7a5d383b584b8eb6dd8ad4d90649a7b9f45a
Georgemoralex <morales.hotch@gmail.com>
2023-10-19 17:24:02 -0700

    3 print alphabet in upper case after

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - 2 print alphabet
+*Description: program to print alphabet
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char abc[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(abc[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
