0843617865d4c68ee542b6f40c5e28bd618d59d0
Georgemoralex <morales.hotch@gmail.com>
2023-10-19 22:54:01 -0700

    alphabet 10x

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_alphabet 10x - task 2-alphabet
+*Description: program to print alphabet 10x
+*Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char abc[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	int j;
+
+	for (j = 0; j < 10; i++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(abc[i]);
+		}
+	_putchar('\n');
+	j = j++;
+	}
+}
