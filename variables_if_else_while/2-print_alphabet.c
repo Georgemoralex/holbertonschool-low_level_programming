857fca34ee8001e2c32ab15f0526fbe0abf8999a
Georgemoralex <morales.hotch@gmail.com>
2023-10-19 17:14:36 -0700

    2 print alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	char abc[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	for (i = 0; i < 27; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(abc[i]);
 	}
-	putchar('\0');
+	putchar('\n');
 	return (0);
 }
