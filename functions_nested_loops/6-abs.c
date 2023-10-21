485e5525ec20d63449cb46ef7fc01f8418ab820c
Georgemoralex <morales.hotch@gmail.com>
2023-10-20 21:55:40 -0700

    6 abs value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_abs - task 6 for holberton project
+*Description: 'program to compute abs value'
+*Return: always 0
+*/
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+		if (n < 0)
+		{
+			return (n*-1)
+		}
+	else
+	{
+		return (0);
+	}
+}
