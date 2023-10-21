f6f1c103f92b517b2cdbfb4c667f068eb065a4f3
Georgemoralex <morales.hotch@gmail.com>
2023-10-20 22:47:39 -0700

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_last_digit - task 7 for holberton project
+*Description: 'program to print last digit'
+*@n: should be an integer
+*Return: return value of last digit
+*/
+int print_last_digit(int n)
+{
+	int lastD;
+	
+	lastD = n % 10;
+	_putchar("%d", lastD);
+	return (lastD);
+}
