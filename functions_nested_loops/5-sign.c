8b682074b665dd8ea6a670ff5209c7093752046c
Georgemoralex <morales.hotch@gmail.com>
2023-10-20 21:03:26 -0700

    5 check sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,21 +2,24 @@
 /**
 *print_sign - task 5 for holberton project
 *Description: 'program to check sign of numer'
-*@c: should be a integer
+*@n: should be a integer
 *Return: return 1 if n is greater than 0, 0 if its 0, -1 if its less than 0
 */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar("+")
 		return (1);
 	}
 		if (n < 0)
 		{
+			_putchar("-")
 			return (-1);
 		}
 		else
 		{
+			_putchar("0")
 			return (0);
 		}
 }
