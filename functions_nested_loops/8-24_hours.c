3be6611a08a6f4ca904cf9f06c1a2f69620f10f9
Georgemoralex <morales.hotch@gmail.com>
2023-10-21 08:36:26 -0700

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*jack_bauer - task 8 for holberton project
+*Description: 'program to display every minute of the day'
+*Return: return 1 if c is lowercase and 0 otherwise
+*/
+void jack_bauer(void)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
