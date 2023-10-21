976a66968e0b76efa75ee25e5149580b913d6071
Georgemoralex <morales.hotch@gmail.com>
2023-10-20 20:43:35 -0700

    3 is lower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 *_islower - check if input char is lowercase
-*Description: program to check if char is lowercase
-*@Parameter: integer c
+*Description: 'program to check if char is lowercase'
+*@parameter: parameter should be a character
 *Return: return 1 if c is lowercase and 0 otherwise
 */
 int _islower(int c)
