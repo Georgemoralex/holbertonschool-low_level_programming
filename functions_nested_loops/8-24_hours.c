dacfa63513a7ba36248a1933354a51927ed1c8e3
Georgemoralex <morales.hotch@gmail.com>
2023-10-21 11:57:02 -0700

    test putchar

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,9 +6,17 @@
 */
 void jack_bauer(void)
 {
-	int t = 00:00;
-	char t[] = "00:00";
+	char t3[] = "0123456789";
+	char t4[] = "0123456789";
+	int n = 0;
+	int k = 0;
 
-	ta = t + 00:01; 
-	_putchar(t[1]);
+	for (n = 0; n < 6; n++)
+	{
+		for(k = 0; k <= 9; k++)
+		{
+			_putchar(t3[n]);
+			_putchar(t4[k]);
+		}
+	}
 }
