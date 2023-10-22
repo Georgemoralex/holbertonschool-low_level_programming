a8cee7a65274b497ead3178810b026ab00099f9a
Georgemoralex <morales.hotch@gmail.com>
2023-10-21 18:22:45 -0700

    10 add 2 num

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add - task 10 add 2 nums
+*Description: 'program to add 2 numbers and return result'
+*@n: should be an integer
+*Return: returns result of the addition
+*/
+int add(int n, int k)
+{
+	return (n + k); 
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int countDigits(int n);
+int add(int, int);
