1efe527b3b4813b906a319ceb1d9a65845e5c78d
Georgemoralex <morales.hotch@gmail.com>
2023-10-28 18:35:23 -0700

    project start

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers - function to print numbers
+*Description: print numbers from 0 to 9
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0, i <=9, i++)
+	{
+		_putchar(i + 48);
+		_putchar('\n');
+	}
+}
