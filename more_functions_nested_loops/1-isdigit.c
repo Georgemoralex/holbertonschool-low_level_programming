615dcf3c84b1f0e7886479ea01113666b6e32bb2
Georgemoralex <morales.hotch@gmail.com>
2023-10-28 18:24:44 -0700

    project start

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 *_isdigit - function similar to isdigit
 *Description: check if character is a digit between 0 and 9 or not
 *@c: is a number to be evaluated
-*Return: 1 if digit else 0 
+*Return: 1 if digit else 0
 */
 int _isdigit(int c)
 {
