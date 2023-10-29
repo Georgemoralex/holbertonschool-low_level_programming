718a0289444d6e241b69afc7349523a47b0a4d72
Georgemoralex <morales.hotch@gmail.com>
2023-10-28 17:56:06 -0700

    project start

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_isupper - function similar to isupper
+*Description: check if character is uppercase or not
+*@c: is a character to be evaluated
+Return: 1 if uppercase else 0 otherwise
+*/
+int _isupper(int c):
+{
+	if (_isupper(c))
+	{
+		return(1)
+	}
+	else
+	{
+		return(0)
+	}
+}
+
---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - 2 print alphabet
+*Description: program to print alphabet
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+        char abc[] = "abcdfghijklmnoprstuvwxyz";
+        int i;
+
+        for (i = 0; i < 24; i++)
+        {
+                putchar(abc[i]);
+        }
+        putchar('\n');
+        return (0);
+}
+
