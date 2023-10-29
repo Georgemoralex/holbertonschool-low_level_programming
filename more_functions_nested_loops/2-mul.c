6b1cba99cf24d1a1dee54bc51675fdbf081ea648
Georgemoralex <morales.hotch@gmail.com>
2023-10-28 18:29:04 -0700

    project start

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*mul - function tu multiply
+*Description: multiply 2 characters
+*@a: first number to be multiplied
+*@b: second number to be multiplied
+*Return: result of the multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
