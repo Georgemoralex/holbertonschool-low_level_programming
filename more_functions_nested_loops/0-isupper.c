9ea838aa684687d76914b630749c010d6a4bd60b
Georgemoralex <morales.hotch@gmail.com>
2023-10-28 18:09:52 -0700

    project start

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,16 @@
 *_isupper - function similar to isupper
 *Description: check if character is uppercase or not
 *@c: is a character to be evaluated
-Return: 1 if uppercase else 0 otherwise
+*Return: 1 if uppercase else 0 otherwise
 */
 int _isupper(int c)
 {
-	if (_isupper(_putchar(c)))
+	if (_isupper(c))
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
-}	
+}
