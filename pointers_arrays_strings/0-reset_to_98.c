0863fbdc33937fc84919864cc445b29f98c9eb68
Georgemoralex <morales.hotch@gmail.com>
2023-11-01 22:08:41 -0700

    pointer

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
 *reset_to_98 - function for a pointer to an integer
 *Description: modifies value to the variable it points to to 98
-*@*n: pointer to n
+*@n: pointer to n
 */
 void reset_to_98(int *n)
 {
