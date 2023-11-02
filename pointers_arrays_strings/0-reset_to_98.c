526a9fdf3de86e6d855d9040683000cb8d8c21f3
Georgemoralex <morales.hotch@gmail.com>
2023-11-01 21:08:28 -0700

    pointer1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*reset_to_98 - function for a pointer to an integer
+*Description: modifies value to the variable it points to to 98
+*@*n: pointer to n
+*/
+void reset_to_98(int *n)
+{
+	int p;
+
+	p = &n
+	*p = 98;
+}
