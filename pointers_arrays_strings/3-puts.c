f5136cd236a48aca1f40e11815287464f7ff8910
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-03 15:25:43 -0700

    pointers2

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -13,3 +13,19 @@ void _puts(char *str)
 			_putchar(str[i]);
 		}
 }
+/**
+ *_strlen - function to calculate lenght
+ *Description: gets the lenght of a string
+ *@s: is a string of characters
+ *Return: the lenght of the string as integer
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
