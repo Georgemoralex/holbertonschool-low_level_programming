185a8d78502ee84be5aa0d37e1c1c254ebc314d1
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-05 01:19:49 -0800

    concatenation1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,28 @@
 #include "main.h"
 /**
-*reset_to_98 - function for a pointer to an integer
-*Description: modifies value to the variable it points to to 98
-*@n: pointer to n
-*/
-void reset_to_98(int *n)
+ * _strncat - concatenates two strings using n bytes src
+ * Description: concatenates two strings using n bytes from src 
+ * @dest: 1st string
+ * @src: 2nd string
+ * @n: bytes from src
+ * Return: the pointer to char
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-	*n = 98;
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
