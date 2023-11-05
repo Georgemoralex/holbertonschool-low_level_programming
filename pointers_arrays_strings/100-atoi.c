b4ddea64bde8ca0db262be7fcf1d251ad30e0437
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-05 01:39:53 -0700

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,18 +1,45 @@
 #include "main.h"
 /**
-*_isdigit - function similar to isdigit
-*Description: check if character is a digit between 0 and 9 or not
-*@c: is a number to be evaluated
-*Return: 1 if digit else 0
+*_atoi - function similar to atoi
+*Description: converts num like string to integer
+*@s: is a string to be evaluated and converted to int
+*Return: integer value or 0 if not valid
 */
-int _isdigit(int c)
+int _atoi(char *s)
 {
-	if (c >= 48 && c <= 57)
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		return (1);
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	else
-	{
+
+	if (f == 0)
 		return (0);
-	}
+
+	return (n);
 }
