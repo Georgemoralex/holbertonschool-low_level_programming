7d01cd74ef195415e3ec049962a01fb6c6769971
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-05 01:23:31 -0800

    concatenation1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _strncat - concatenates two strings using n bytes src
- * Description: concatenates two strings using n bytes from src 
- * @dest: 1st string
- * @src: 2nd string
- * @n: bytes from src
- * Return: the pointer to char
- */
+* _strncat - concatenates two strings using n bytes src
+* Description: concatenates two strings using n bytes from src
+* @dest: 1st string
+* @src: 2nd string
+* @n: bytes from src
+* Return: the pointer to char
+*/
 char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
