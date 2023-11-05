2603ded6c59e353a8e46550b9f625e1a53385662
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-04 22:49:35 -0700

    pointers9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,33 @@
 #include "main.h"
 /**
-*_isdigit - function similar to isdigit
+**_strcpy - function similar to isdigit
 *Description: check if character is a digit between 0 and 9 or not
-*@c: is a number to be evaluated
-*Return: 1 if digit else 0
+*@dest: destination string
+*@src: source string
+*Return: pointer to dest
 */
-int _isdigit(int c)
+char *_strcpy(char *dest, char *src)
 {
-	if (c >= 48 && c <= 57)
+	int length = _strlen(src);
+	int i;
+
+	for (i = 0; i < length; i++)
 	{
-		return (1);
+		dest[i] = src[i]
 	}
-	else
+}
+/**
+ *_strlen - function to calculate lenght
+ *Description: gets the lenght of a string
+ *@s: is a string of characters
+ *Return: the lenght of the string as integer
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		return (0);
 	}
+	return (i);
 }
