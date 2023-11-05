70831b5c21dc6eda32f895ff5513e7386e3140f5
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-04 17:15:06 -0700

    pointers7

--- pointers_arrays_strings/7test.c
+++ pointers_arrays_strings/7test.c
@@ -9,12 +9,25 @@ void puts_half(char *str)
 	int i;
 	int length = _strlen(str);
 	int half = length / 2;
+	int half_odd = (length - 1) / 2;
+	int reminder = length % 2;
 
+	if ((reminder == 0))
+	{
 		for (i = half; i < length; i++)
 		{
 			_putchar(str[i]);
 		}
 		_putchar(10);
+	}
+	else
+	{
+		for (i = half_odd; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar(10);
+	}
 }
 /**
  *_strlen - function to calculate lenght
