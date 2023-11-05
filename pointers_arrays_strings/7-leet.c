46a11bc844242d83548879e2f4eb59f22e3994cd
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-05 01:56:31 -0800

    concatenation1

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,10 +1,29 @@
 #include "main.h"
 /**
-*reset_to_98 - function for a pointer to an integer
-*Description: modifies value to the variable it points to to 98
-*@n: pointer to n
+* leet - string encoder
+* Description: encodes a string into 1337
+* @s: bytes from src
+* Return: encoded string
 */
-void reset_to_98(int *n)
+char *leet(char *s)
 {
-	*n = 98;
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
 }
