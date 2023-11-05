efadf4535eb34de60df62c059c0a947d76b4c313
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-05 01:35:57 -0800

    concatenation1

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,22 @@
 #include "main.h"
 /**
-*reset_to_98 - function for a pointer to an integer
-*Description: modifies value to the variable it points to to 98
-*@n: pointer to n
+* _strcmp - string comparator
+* Description: conpares string 1 and string 2
+* @s1: 1st string
+* @s2: 2nd string
+* Return: an integer
 */
-void reset_to_98(int *n)
+int _strcmp(char *s1, char *s2)
 {
-	*n = 98;
+	int i = 0, op = 0;
+
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+
+	return (op);
 }
