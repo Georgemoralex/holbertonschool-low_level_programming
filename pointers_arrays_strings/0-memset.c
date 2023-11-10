d4330bab9bb824a388845efbf980c4943a235922
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-09 20:55:54 -0800

    more pointers

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 *_memset - function to fill memory with a constant byte
-*Description: fills the first n bytes of the memory area pointed to by s with the constant byte b
-*@a: char type 
+*Description: fills 1st n bytes of mem area pointed 2 by s with const byte b
+*@s: char type
 *@b: char type
 *@n: integer type
 *Return: a pointer to the memory area S
