736463d08e9c9f244132e0097425726a4fed36d0
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-11 20:38:49 -0800

    7 pointers

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,18 +1,30 @@
 #include "main.h"
 /**
-*swap_int - function to swap
-*Description: swaps the value between 2 numbers
-*@a: first number
-*@b: second number
+*print_diagsums - sum array diagonals
+*Description: sums the 2 main diagonals of the given array
+*@a: pointer to an array of integers
+*@size: size of the array
+*Return: void
 */
-void swap_int(int *a, int *b)
+void print_diagsums(int *a, int size)
 {
-	int i;
-	int j;
+	int counterUp = 0;
+	int counterDown = size - 1;
+	int row = 0;
+	int column = 0;
+	int sumUp = 0;
+	int sumDown = 0;
 
-	i = *a;
-	j = *b;
-
-	*a = j;
-	*b = i;
+	while (row < size)
+	{
+		sumUp += *(a + row*size + row);
+		row++;
+	}
+	row = 0;
+	while (row < size)
+	{
+		sumDown += *(a + row * size + (size - row - 1));
+		row++;
+	}
+	printf("%d, %d\n", sumUp, sumDown);
 }
