06878dc5f04cb1e7963a81a4293840b4e69d980e
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-12 12:39:08 -0800

    1st recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * _puts_recursion: prints string
+ * Description: gets a pointer to character and prints it
+ * @s: string to be printed
+ * Return: nothing to return
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+}
