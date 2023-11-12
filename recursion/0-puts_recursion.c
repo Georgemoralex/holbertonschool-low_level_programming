517c5c1b5d6b34a21be61f962082b4ffeef49dea
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-12 13:03:04 -0800

    1st recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s > 'z')
+	if (*s == '\0')
 	{
 		return;
 	}
-	_putchar(*s);
-	_puts_recursion(char *(s + 1));
+	_putchar(s);
+	s++;
+	_puts_recursion(char *s);
 }
