72f914f039620c3d0f482dba33a052ade012ed90
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-12 11:08:37 -0800

    haystack

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -5,31 +5,32 @@
 *Description: finds the first occurence of the needle in haystack
 *@haystack: where needle will be found
 *@needle: string to be found in haystack
+*Return: return address of needle in haystack or null if it isnt found
 */
 char *_strstr(char *haystack, char *needle)
 {
 	int hLength = 0;
 	int nLength = 0;
 
-	if (haystack == NULL || needle == NULL) 
+	if (haystack == NULL || needle == NULL)
 	{
-		return NULL; 
+		return NULL;
 	}
 
-	while (haystack[hLength] != '\0') 
+	while (haystack[hLength] != '\0')
 	{
-		nLength = 0; 
+		nLength = 0;
 
-		while (needle[nLength] != '\0' && haystack[hLength + nLength] == needle[nLength]) 
+		while (needle[nLength] != '\0' && haystack[hLength + nLength] == needle[nLength])
 		{
 			nLength++;
 		}
 
-		if (needle[nLength] == '\0') 
+		if (needle[nLength] == '\0')
 		{
-			return &haystack[hLength]; 
+			return &haystack[hLength];
 		}
 		hLength++;
 	}
-	return NULL; 
+	return NULL;
 }
