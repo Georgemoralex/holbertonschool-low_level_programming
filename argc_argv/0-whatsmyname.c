bdb7de5d01dddea0a7aa20e19b41309d0751d4ff
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-17 01:29:40 -0600

    task2 1st try

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-*main - gets length of a prefix string
-*Description: gets # of chars of 1st string that matches the chars in 2d string
+*main - prints the name of the program
+*Description: if args is > 0, then prints name of program
 *@argc: integer with count of arguments supplied to the program
 *@argv: array of pointers to the string for argc arguments
 *Return: 0 for success
--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 /**
-*main - gets length of a prefix string
-*Description: gets # of chars of 1st string that matches the chars in 2d string
+*main - prints number of args
+*Description: prints all arguments passed into it
 *@argc: integer with count of arguments supplied to the program
 *@argv: array of pointers to the string for argc arguments
 *Return: 0 for success 
 */
 int main(int argc, char *argv[])
 {
-	
+    int i;
+
+    if (argc > 0)
+    {
+        for (i = 0, i < argc , i++)
+            printf("%s\n",argv[i]);
+    }
+    return (0);
 }
