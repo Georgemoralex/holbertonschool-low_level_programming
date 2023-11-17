032857df1451b0c4bb6786ecc9cf30381a4c277c
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-17 02:56:45 -0600

    upload

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <stddef.h>
 /**
-*main - gets length of a prefix string
-*Description: gets # of chars of 1st string that matches the chars in 2d string
+*main - prints multiplication of args
+*Description: prints multiplication of 2 args passed into it
 *@argc: integer with count of arguments supplied to the program
 *@argv: array of pointers to the string for argc arguments
-*Return: 0 for success 
+*Return: 0 for success and 1 for error
 */
 int main(int argc, char *argv[])
 {
-	
+	/*a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    result = a * b;
+    */
+    
+    if (argv != NULL && argc > 2)
+	{
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2]));
+        return (0);
+    }
+	else
+    {
+        printf("Error\n");
+        return (1);
+    }
 }
