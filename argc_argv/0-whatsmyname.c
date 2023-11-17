0db01047826ca05e1434bc36ba23b8dbc114870b
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-17 01:00:52 -0600

    argc and argv start

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - gets length of a prefix string
+*Description: gets # of chars of 1st string that matches the chars in 2d string
+*@argc: integer with count of arguments supplied to the program
+*@argv: array of pointers to the string for argc arguments
+*Return: 0 for success 
+*/
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+    return (0);
+}
