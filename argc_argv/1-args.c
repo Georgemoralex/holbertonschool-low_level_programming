c9acf22c43c35913be408005b6972f5d59a9ab08
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-17 01:39:55 -0600

    try

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -10,10 +10,11 @@ int main(int argc, char *argv[])
 {
     int i;
 
-    if (argc >= 0)
+    if (argc > 1)
     {
         for (i = 1; i < argc; i++)
             printf("%s\n",argv[i]);
     }
-    return (0);
+    printf(0);
+    //return (0);
 }
