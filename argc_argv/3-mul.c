e435cc2b4a3d927698b8ac00e9c8d96690d27573
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-17 01:11:05 -0800

    doslines

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,20 +8,20 @@
 *Return: 0 for success and 1 for error
 */
 int main(int argc, char *argv[])
-{   
-    int a, b, result;
-    
-    if (argc > 2)
+{
+	int a, b, result;
+
+	if (argc > 2)
 	{
-        a = atoi(argv[1]);
-        b = atoi(argv[2]);
-        result = a * b;
-        printf("%d\n", result);
-        return (0);
-    }
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		result = a * b;
+		printf("%d\n", result);
+		return (0);
+	}
 	else
-    {
-        printf("Error\n");
-        return (1);
-    }
+	{
+	printf("Error\n");
+	return (1);
+	}
 }
