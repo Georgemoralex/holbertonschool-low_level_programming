af932da17742b2e2b946ced800137d03e39fb5c1
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-16 23:22:42 -0800

    erase spaces

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,13 +4,13 @@
 *Description: gets # of chars of 1st string that matches the chars in 2d string
 *@argc: integer with count of arguments supplied to the program
 *@argv: array of pointers to the string for argc arguments
-*Return: 0 for success 
+*Return: 0 for success
 */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
