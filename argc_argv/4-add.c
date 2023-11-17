86e58408bc915c1b97fdfe039fd6b5b73bcc05d4
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-17 03:51:02 -0600

    s

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
-*main - gets length of a prefix string
-*Description: gets # of chars of 1st string that matches the chars in 2d string
+*main - prints adds of positive numbers
+*Description: only positive numbers are added, all others are erros
 *@argc: integer with count of arguments supplied to the program
 *@argv: array of pointers to the string for argc arguments
-*Return: 0 for success 
+*Return: 0 for success and 1 for error
 */
 int main(int argc, char *argv[])
-{
-	
+{   
+    int sum;
+    int i;
+    
+    if (argc > 2)
+    {
+        for (i = 1; i < argc; i++)
+        {
+            if (isdigit(argv[i]) == 0)
+            {
+                if (atoi(argv[i]) > 0)
+                {
+                    sum += atoi(argv[i]);
+                }
+            }
+            else
+            {
+                prinf("Error\n");
+                return (1);
+            }
+        }
+    }
+    else
+    {
+        printf("0\n");
+    }
+    printf("%d", sum;
 }
