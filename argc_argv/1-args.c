29933b6e3c2ff0576c8da6254c4124fbe82ca8fd
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-17 00:34:52 -0800

    dos lines

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,13 +5,13 @@
 *Description: prints all arguments passed into it
 *@argc: integer with count of arguments supplied to the program
 *@argv: array of pointers to the string for argc arguments
-*Return: 0 for success 
+*Return: 0 for success
 */
 int main(int argc, char *argv[])
 {
-    if (argv != NULL)
-    {
-        printf("%d\n", (argc - 1));
-    }
-    return (0);
+	if (argv != NULL)
+	{
+		printf("%d\n", (argc - 1));
+	}
+	return (0);
 }
