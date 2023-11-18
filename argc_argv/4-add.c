bc03be939c2e18f0ed3ed8415deda05d7bf38d88
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-18 11:39:48 -0600

    betty style

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,43 +11,43 @@
 */
 int main(int argc, char *argv[])
 {   
-    int i, j, k, s1, sum = 0;
+int i, j, k, s1, sum = 0;
 
-  if (argc > 1)
-    {
-        for (i = 1; i < argc; i++)
-        {
-          if (argv[i][0] == '-')
-          {
-            s1 = strlen(argv[i]);
-            for (k = 1; k < s1; k++)
-              {
-                if (isdigit(argv[i][k]) == 0)
-                {
-                  printf("Error\n");
-                  return (1);
-                }
-              }
-          }
-          else
-          {
-            s1 = strlen(argv[i]);
-            for (j = 0 ; j < s1; j++)
-            {
-                if (isdigit(argv[i][j]) == 0)
-                {
-                  printf("Error\n");
-                  return (1);
-                }
-            }
-          }
-          
-          if (atoi(argv[i]) > 0)
-            {
-              sum += atoi(argv[i]);
-            }
-        }
-    } 
-    printf("%d\n", sum);
-    return (0);
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+if (argv[i][0] == '-')
+{
+s1 = strlen(argv[i]);
+for (k = 1; k < s1; k++)
+{
+if (isdigit(argv[i][k]) == 0)
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+else
+{
+s1 = strlen(argv[i]);
+for (j = 0 ; j < s1; j++)
+{
+if (isdigit(argv[i][j]) == 0)
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+
+if (atoi(argv[i]) > 0)
+{
+sum += atoi(argv[i]);
+}
+}
+} 
+printf("%d\n", sum);
+return (0);
 }
