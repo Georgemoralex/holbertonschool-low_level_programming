276c926dfa58e902023dbdaa69c6a0c4deb65e2a
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-20 05:12:45 -0800

    m

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -11,67 +11,67 @@
 
 int main(int argc, char *argv[])
 {
-    int coins[] = {25, 10, 5, 2, 1};
-    int result[5];
-    int i, mod = 1, modOp, smallest, cents, total = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int result[5];
+	int i, mod = 1, modOp, smallest, cents, total = 0;
 
-    if (argc > 2 || argc == 1)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc > 2 || argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    if (*argv[1] == '-')
-    {
-        printf("0\n");
-        return (0);
-    }
-    cents = atoi(argv[1]);
-    smallest = cents;
+	if (*argv[1] == '-')
+	{
+		printf("0\n");
+		return (0);
+	}
+	cents = atoi(argv[1]);
+	smallest = cents;
 
-    if (cents == 0)
-    {
-        printf("%d\n", total);
-        return (0);
-    }
+	if (cents == 0)
+	{
+		printf("%d\n", total);
+		return (0);
+	}
 
-    while (mod != 0)
-    {
-        if (cents != 1)
-        {
-            for (i = 0; i < 5; i++)
-            {
-                result[i] = cents / coins[i];
-                if (result[i] < 1)
-                {
-                    result[i] = 0;
-                }
-                if (i == 0 && result[i] != 0)
-                {
-                    smallest = result[i];
-                    modOp = i;
-                }
-                else
-                {
-                    if (result[i] < smallest && result[i] != 0)
-                    {
-                        smallest = result[i];
-                        modOp = i;
-                    }
-                }
-            }
-        }
-        else
-        {
-            total += 1;
-            printf("%d\n", total);
-            return (0);
-        }
-        mod = cents % coins[modOp];
-        cents = mod;
-        total += result[modOp];
-        smallest = cents;
-    }
-    printf("%d\n", total);
-    return(0);
+	while (mod != 0)
+	{
+		if (cents != 1)
+		{
+			for (i = 0; i < 5; i++)
+			{
+				result[i] = cents / coins[i];
+				if (result[i] < 1)
+				{
+					result[i] = 0;
+				}
+				if (i == 0 && result[i] != 0)
+				{
+					smallest = result[i];
+					modOp = i;
+				}
+				else
+				{
+					if (result[i] < smallest && result[i] != 0)
+					{
+						smallest = result[i];
+						modOp = i;
+					}
+				}
+			}
+		}
+		else
+		{
+			total += 1;
+			printf("%d\n", total);
+			return (0);
+		}
+		mod = cents % coins[modOp];
+		cents = mod;
+		total += result[modOp];
+		smallest = cents;
+	}
+	printf("%d\n", total);
+	return(0);
 }
