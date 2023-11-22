f1106d6612494fae2ef3e0d754aac788e334a4f7
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-22 02:03:39 -0600

    s

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,12 +19,12 @@ char *str_concat(char *s1, char *s2)
     printf("%d", s1[0]);
     if (s1 == NULL)
     {
-        s1[0] = '\0';
+        s1 = "";
     }
 
     if (s2 == NULL)
     {
-        s2[0] = '\0';
+        s2 = "";
     }
 
     while (*(s1 + i) != '\0')
