0dcafe95ad8e3059b7fa7e5f4db2a85d3aa280f2
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-22 02:53:57 -0600

    s

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,31 +11,59 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-    
-    int t = 1;
-    int r = 2;
+    char *array;
+    int i = 0, j = 0, t = 0;
+    int k, l;
+
+    int w = 1;
+    int y = 2;
    
 
     
     if (s1 == NULL)
     {
         s1 = "";
-        printf("%d\n", t);
+        printf("%d\n", w);
     }
 
     if (s2 == NULL)
     {
         s2 = "";
-        printf("%d\n", r);
+        printf("%d\n", y);
     }
 
     printf("%s\n%s\n", s1, s2);
 
+    while (*(s1 + i) != '\0')
+    {
+        i++;
+    }
+    while (*(s2 + j) != '\0')
+    {
+        j++;
+    }
 
-  
+    t = i + j;
+    array = malloc((t + 1) * sizeof(char));
 
+    if (array == NULL)
+    {
+        return (NULL);
+    }
+
+    for (k = 0; k < i; k++)
+    {
+        array[k] = s1[k];
+    }
     
-    return (0);
+    for (l = 0; l < j; l++)
+    {
+        array[k] = s2[l];
+        k++;
+    }
+    array[k] = '\0';
+
+    return (array);
 
 
 }
