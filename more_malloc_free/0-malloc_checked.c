ea17c9f07d95a42f3d3bf806d37e71849be15789
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-26 12:51:25 -0600

    parentheses missing

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -13,8 +13,10 @@ void *malloc_checked(unsigned int b)
     int *array;
 
     array = malloc(b);
-    if array == NULL
+    if (array == NULL)
+    {
         exit (98);
+    }
     else
     {
         return (array);
