39f745e747b70cb5afb213d876fba117875c0102
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-26 13:30:45 -0600

    s

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
 /**
 *malloc_checked - function to allocate memory
---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*string_nconcat - Write a function that concatenates two strings.
+*Description: The returned pointer shall point to a newly allocated space in memory
+*@s1: string 1
+*@s2: string 2
+*@n: n bytes of the second string
+*Return: pointer to allocated memory
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    char *conc;
+    unsigned int length_s1, length_s1, length_conc;
+    unsigned int i;
+
+    if (s1 == NULL)
+    {
+        s1 == "";
+    }
+    if (s2 == NULL)
+    {
+        s2 == "";
+    }
+    for (length_s1 = 0; s1[length_s1] != '\0'; length_s1++)
+    for (length_s2 = 0; s1[length_s2] != '\0'; length_s2++)
+    if (n > length_s2)
+    {
+        n = length_s2;
+    }
+    length_conc = length_s1 + n;
+    conc = malloc(length_conc + 1);
+    if (conc == NULL)
+    {
+        return (NULL);
+    }
+    for (i =0; i < length_conc; i++)
+        if (i < length_s1)
+        {
+            conc[i] = s1[i];
+        }
+        else
+        {
+            conc[i] = s2[i - length_s1];
+        }
+    cont[i] = '\0';
+    return (conc);
+}
