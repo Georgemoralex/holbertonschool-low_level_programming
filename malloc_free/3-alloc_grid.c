7412e674c445b4e3dad5b3f927f3603d37a31d3c
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-25 22:23:55 -0600

    s

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -18,39 +18,30 @@ int **alloc_grid(int width, int height)
     {
         return (NULL);
     }
-
     array = malloc(sizeof(int *) * height);
     if (array == NULL)
     {
-        free(array);
         return (NULL);
     }
-    else
+    for (i = 0; i < height; i++)
     {
-        for (i = 0; i < height; i++)
+        array[i] = malloc(sizeof(int) * width);
+    
+        if (array[i] == NULL)
         {
-            array[i] = malloc(sizeof(int) * width);
-        
-            if (array[i] == NULL)
+            for (j = 0; j < i; j++)
             {
-                free(array[i]);
-                return (NULL);
+                free(array[j]);
             }
-        }
-        if (array == NULL)
-        {
             free(array);
             return (NULL);
         }
-        else
+    }
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j < width; j++)
         {
-            for (i = 0; i < height; i++)
-            {
-                for (j = 0; j < width; j++)
-                {
-                    array[i][j] = 0;
-                }
-            }
+            array[i][j] = 0;
         }
     }
     return (array);
