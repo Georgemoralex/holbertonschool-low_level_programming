e2e482ef1d7106e638fa528a1087ce329675d70a
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-26 12:48:06 -0600

    more malloc free

---
+++ more_mallo_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+*malloc_checked - function to allocate memory
+*Description: allocates mem and return a status equals to 98 if function fails
+*@b: size of the array to be dinamically allocated
+*Return: pointer to allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+    int *array;
+
+    array = malloc(b);
+    if array == NULL
+        exit (98);
+    else
+    {
+        return (array);
+    }
+}
