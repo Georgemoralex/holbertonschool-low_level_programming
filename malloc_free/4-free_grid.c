d6f4f0099e646f60bb32e4d98101a10a12f18cb4
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-25 22:39:19 -0600

    s

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+*free_grid - free space reserved from our alloc grid function
+*Description: frees 2D array previously created by alloc_grid function
+*@grid: 2d array that will be freed
+*@height: height of the array to be freed
+*Return: void nothing to return
+*/
+void free_grid(int **grid, int height)
+{
+    int i;
+
+    for (i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+}
