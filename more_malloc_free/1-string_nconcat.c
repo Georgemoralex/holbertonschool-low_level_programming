55016ac051cc1a648cb0a7de99cb6d25bb5cc28d
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-26 20:18:45 -0600

    s

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -24,11 +24,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     }
     for (length_s1 = 0; s1[length_s1] != '\0'; length_s1++)
     for (length_s2 = 0; s2[length_s2] != '\0'; length_s2++)
-    if (n > length_s2)
-    {
-        n = length_s2;
-    }
-    length_conc = length_s1 + n;
+    
+    length_conc = length_s1 + length_s2;
+    
     conc = malloc(length_conc + 1);
     if (conc == NULL)
     {
