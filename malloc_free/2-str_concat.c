acb1267ca5d972ffa8f4e78915c231307ab69b14
GeorgeMoralex <morales.hotch@gmail.com>
2023-11-26 20:08:54 -0800

    format

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -30,8 +30,8 @@ char *str_concat(char *s1, char *s2)
 	for (k = 0; k < i; k++)
 		array[k] = s1[k];
 	for (l = 0; l < j; l++)
-		array[k] = s2[l];
-		k++;
+	{array[k] = s2[l];
+	k++;}
 	array[k] = '\0';
 	return (array);
 }
