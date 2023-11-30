3b3102d9348809b3a05586044bb452087426c17a
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-11-29 23:08:15 -0600

    s

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 /**
+*main: program to print name of the file it was compiled from
 *Description: shows the name of the program it was compiled from
+*return: 0 if success
 */
 
 int main(void)
