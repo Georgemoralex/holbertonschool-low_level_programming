14c1f5de7ee111e66b847385ca6340bd123985c0
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-01 00:15:05 -0600

    s

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG
 #define DOG
 
+/**
+ *struct dog - struct that shows name, age and owner of puppy
+ *Description: dog structure with the information of a puppy
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog 
+ */
+
 struct dog
 {
 	char *name;
