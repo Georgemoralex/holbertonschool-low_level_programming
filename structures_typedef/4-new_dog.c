6f449c42c76518546db6ce6dc5c3f55fd4b0b731
GeorgeMoralex <morales.hotch@gmail.com>
2023-12-02 14:25:35 -0800

    typedef structs

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,49 +13,56 @@
 *Return: pointer to dog_t or NULL if it fails
 */
 
-dog_t *new_dog(char *name, float age, char *owner) {
-    dog_t *pointerD;
-    int i, length_name, length_owner;
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *pointerD;
+	int i, length_name, length_owner;
 
-    pointerD = malloc(sizeof(dog_t));
-    if (pointerD == NULL || name == NULL || owner == NULL) {
-        free(pointerD);
-        return NULL;
-    }
+	pointerD = malloc(sizeof(dog_t));
+	if (pointerD == NULL || name == NULL || owner == NULL)
+	{
+		free(pointerD);
+		return (NULL);
+	}
+	
+	length_name = 0;
+	while (name[length_name] != '\0')
+	{
+		length_name++;
+	}
+	
+	length_owner = 0;
+	while (owner[length_owner] != '\0')
+	{
+		length_owner++;
+	}
 
-    length_name = 0;
-    while (name[length_name] != '\0') {
-        length_name++;
-    }
+	pointerD->name = malloc(length_name + 1);
+	pointerD->owner = malloc(length_owner + 1);
 
-    length_owner = 0;
-    while (owner[length_owner] != '\0') {
-        length_owner++;
-    }
+	if (pointerD->name == NULL || pointerD->owner == NULL) 
+	{
+		free(pointerD->owner);
+		free(pointerD->name);
+		free(pointerD);
+		return (NULL);
+	}
 
-    pointerD->name = malloc(length_name + 1);
-    pointerD->owner = malloc(length_owner + 1);
+	for (i = 0; i < length_name; i++) 
+	{
+		pointerD->name[i] = name[i];
+	}
+	pointerD->name[i] = '\0';
 
-    if (pointerD->name == NULL || pointerD->owner == NULL) {
-        free(pointerD->owner);
-        free(pointerD->name);
-        free(pointerD);
-        return NULL;
-    }
+	pointerD->age = age;
 
-    for (i = 0; i < length_name; i++) {
-        pointerD->name[i] = name[i];
-    }
-    pointerD->name[i] = '\0';
+	for (i = 0; i < length_owner; i++)
+	{
+		pointerD->owner[i] = owner[i];
+	}
+	pointerD->owner[i] = '\0';
 
-    pointerD->age = age;
-
-    for (i = 0; i < length_owner; i++) {
-        pointerD->owner[i] = owner[i];
-    }
-    pointerD->owner[i] = '\0';
-
-    return pointerD;
+	return (pointerD);
 }
 
 #endif
