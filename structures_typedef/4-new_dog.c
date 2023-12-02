7f2bd6332b9e1d7ce6c0e3bf672dbfa2513b646d
GeorgeMoralex <morales.hotch@gmail.com>
2023-12-02 15:07:17 -0800

    typedef structs

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -20,48 +20,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	pointerD = malloc(sizeof(dog_t));
 	if (pointerD == NULL || name == NULL || owner == NULL)
-	{
 		free(pointerD);
 		return (NULL);
-	}
-	
 	length_name = 0;
 	while (name[length_name] != '\0')
-	{
 		length_name++;
-	}
-	
 	length_owner = 0;
 	while (owner[length_owner] != '\0')
-	{
 		length_owner++;
-	}
-
 	pointerD->name = malloc(length_name + 1);
 	pointerD->owner = malloc(length_owner + 1);
-
 	if (pointerD->name == NULL || pointerD->owner == NULL) 
-	{
 		free(pointerD->owner);
 		free(pointerD->name);
 		free(pointerD);
 		return (NULL);
-	}
-
 	for (i = 0; i < length_name; i++) 
-	{
 		pointerD->name[i] = name[i];
-	}
 	pointerD->name[i] = '\0';
-
 	pointerD->age = age;
-
 	for (i = 0; i < length_owner; i++)
-	{
 		pointerD->owner[i] = owner[i];
-	}
 	pointerD->owner[i] = '\0';
-
 	return (pointerD);
 }
 
