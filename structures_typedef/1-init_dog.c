de211dac4e3aa2c99740645f06683a639ea4f44d
GeorgeMoralex <morales.hotch@gmail.com>
2023-12-02 14:41:49 -0800

    typedef structs

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -6,7 +6,7 @@
 /**
  *init_dog - Write a function that initialize a variable of type struct dog
  *Description: function that inits var and prints dog name and age
- *@d: struct dog.
+ *@d: struct dog
  *@name: name of the dog.
  *@age: age of the dog.
  *@owner: owner of the dog.
