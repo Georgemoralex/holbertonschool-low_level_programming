4e2aba69ed7b079e5004ce555e4a34225cf79820
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-02 16:06:24 -0600

    s

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
 
 /**
  *struct dog - struct that shows name, age and owner of puppy
@@ -19,6 +19,6 @@ struct dog
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
