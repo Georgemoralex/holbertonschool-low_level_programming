757196071bbf7547ed975c1925645f31926d92b8
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-02 16:10:13 -0600

    s

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#ifndef DOG
+#define DOG
+#include "dog.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+*new_dog - function to free dogs
+*Description: function to free all fields
+*@d: dog_t struct
+*Return: void
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
