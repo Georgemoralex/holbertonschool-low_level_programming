60bc3dcdeee22bf3b4a38238550b76a1b7687cca
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-06 20:03:46 -0600

    s

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,16 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
 *print_name - function that prints a name
 *Description: function that prints a name
-*@name: name of dog
-*@char: age of dog
+*@name: name to be printed
+*@f: name of the function
 *Return: void
 */
 
 void print_name(char *name, void (*f)(char *)) {
+
+if (name == NULL || f == NULL)
+    return (NULL);
 (*f)(name);
 }
