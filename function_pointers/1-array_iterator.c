c58c32d84e9f006ffd0c373ef3ed71389c0526f9
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-09 13:06:48 -0600

    s

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,9 +14,8 @@ void array_iterator(int *array, size_t size, void (*action)(int)) {
 
 size_t i;
 
-if (size <= 0 || array == NULL || argv[2] == NULL)
+if (size <= 0 || array == NULL || action == NULL)
     exit(1);
 for (i = 0; i < size; i++)
     (*action)(array[i]);
-
 }
