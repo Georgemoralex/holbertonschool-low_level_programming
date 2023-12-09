2ee2202a6879f76a660f8a267f90b33113d829e6
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-09 16:57:29 -0600

    s

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+*int_index - searchs an integer in an array
+*Description: returns array position that meets criteria
+*@array: pointer to array
+*@size: size of array
+*@cmp: name of the function
+*Return: position of the array that meets criteria
+*/
+
+int int_index(int *array, int size, int (*cmp)(int)) {
+
+int i = 0;
+int c = 0;
+
+if (array == NULL || cmp == NULL)
+    exit(1);
+if (size <= 0)
+    return (-1);
+
+while (c = 0)
+{
+    c = (*cmp)(array[i]);
+    i++;
+}
+return (i);
+}
