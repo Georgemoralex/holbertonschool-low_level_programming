04e4afb37fa39a06808c2e9d8a36c63fb7ec254b
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-10 03:36:36 -0600

    s

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,37 +14,19 @@ int main(int argc, char *argv[]) {
 int calc;
 
 if (argc != 4) {
-        _putchar(69);
-        _putchar(114);
-        _putchar(114);
-        _putchar(111);
-        _putchar(114);
-        _putchar(10);
+        printf("Error\n");
         exit(98);
-    }
-if (argv[2][0] != '+' &&
-    argv[2][0] != '-' &&
-    argv[2][0] != '/' &&
-    argv[2][0] != '*' &&
-    argv[2][0] != '%' &&
-    argv[2][0] != '\0') {
-        _putchar(69);
-        _putchar(114);
-        _putchar(114);
-        _putchar(111);
-        _putchar(114);
-        _putchar(10);
+}
+if (argv[2][1] != '\0' || (argv[2][0] != '+' && argv[2][0] != '-' &&
+                                argv[2][0] != '*' && argv[2][0] != '/' &&
+                                argv[2][0] != '%')) {
+        printf("Error\n");
         exit(99);
 }
 if (atoi(argv[3]) == 0) {
-        _putchar(69);
-        _putchar(114);
-        _putchar(114);
-        _putchar(111);
-        _putchar(114);
-        _putchar(10);
-        exit(100);
-    }
+    printf("Error\n");
+    exit(100);
+}
 calc = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 printf("%d\n", calc);
 return(0);
