7f96ad2508abebc3419214d7e931cee569f65e6c
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-10 04:18:27 -0600

    variadic functions

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+*sum_them_all - sum all parameters
+*Description: sum of all function parameters
+*@n: number of args
+*Return: sum of arguments
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(valist);
+
+	return (sum);
+}
