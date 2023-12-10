fd6b6b46b2cff1975c776cd9710c5925506402f7
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-10 13:52:38 -0600

    s

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,11 +18,15 @@ if (argc != 4)
 	printf("Error\n");
 	exit(98);
 }
-if (argv[2][1] && get_op_func(argv[2]) == NULL)
+if ((argv[2][0] != '+' &&
+		argv[2][0] != '-' &&
+		argv[2][0] != '*' &&
+		argv[2][0] != '/' &&
+		argv[2][0] != '%') && argv[2][0] != '.')
 {
 	printf("Error\n");
 	exit(99);
-}
+}		 
 if (atoi(argv[3]) == 0)
 {
 	printf("Error\n");
