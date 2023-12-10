31bc57b7d7b074107b971c29d1a07da1d05acce0
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-10 13:21:50 -0600

    s

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,7 +18,7 @@ if (argc != 4)
 	printf("Error\n");
 	exit(98);
 }
-if (argv[2][1] != '\0' || (argv[2][0] != '+' &&	argv[2][0] != '-')
+if (argv[2][1] != '\0' || (argv[2][0] != '+' &&	argv[2][0] != '-'))
 {
 	printf("Error\n");
 	exit(99);
