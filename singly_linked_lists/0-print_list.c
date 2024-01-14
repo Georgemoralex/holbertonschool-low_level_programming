1e60f8c98ada69ae94fa38db7ef7e679ad9aec05
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-14 00:47:31 -0600

    s

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 /**
  * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
+ * @Nhead: singly linked list.
  * Return: number of elements in the list.
  */
 
