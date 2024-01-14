d3be6da98a1b2ad24dba78634ede01e7682e5baf
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-14 00:33:06 -0600

    linked lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *Nhead)
+{
+	size_t elements;
+
+	elements = 0;
+	while (Nhead != NULL)
+	{
+		if (Nhead->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", Nhead->len, Nhead->str);
+		Nhead = Nhead->next;
+		elements++;
+	}
+	return (elements);
+}
