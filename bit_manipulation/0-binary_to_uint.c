f890dbab2053125ce4d5afe0c97b35c892f76eb2
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-19 01:12:59 -0600

    s

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -12,8 +12,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while(*b != '\0')
-	{
+	while(*b != '\0') {
 		if (*b > '1')
 		return (0);
 		newTotal = (newTotal * 2) + *b - 48;
