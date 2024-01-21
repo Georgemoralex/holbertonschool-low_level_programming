117280cef9b59c7fab9771ca2d724efad3198061
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-21 03:58:58 -0600

    s

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int get_bit(unsigned long int n, unsigned int index) {
+
+	if (index >= sizeof(unsigned long int) * 8) {
+		return -1;
+	}
+	unsigned long int mask = 1UL << index;
+	if ((n & mask) == 0) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
