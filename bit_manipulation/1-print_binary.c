245e0ff161267b47da641779008b88dbdf69856e
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-21 03:40:18 -0600

    s

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -8,23 +8,25 @@
 
 void print_binary(unsigned long int n)
 {
-    int length = 0;
-    int x;
+	int length = 0;
+	int x;
 
-    unsigned long int temp = n;
-    while (temp > 0)
-    {
-        temp >>= 1;
-        length += 1;
-    }
-
-    if (length == 0) {
-        _putchar('0');
-    } else {
-        for (x = length - 1; x >= 0; x--)
-        {
-            int bit = (n >> x) & 1;
-            _putchar(bit + '0');
-        }
-    }
+	unsigned long int temp = n;
+	while (temp > 0)
+	{
+		temp >>= 1;
+		length += 1;
+	}
+	if (length == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		for (x = length - 1; x >= 0; x--)
+		{
+			int bit = (n >> x) & 1;
+			_putchar(bit + '0');
+		}
+	}
 }
