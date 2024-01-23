f0cc4c644d86f3c91b5460a08bf8e2140b2bc8dc
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-22 19:12:19 -0600

    s

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,16 +17,42 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
+	int original_stdout = dup(STDOUT_FILENO);
+	if (original_stdout == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	// Redirect STDOUT to STDERR
+	if (dup2(STDERR_FILENO, STDOUT_FILENO) == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
 	while (letters > 0 && read(fd, &c, 1) == 1)
 	{
-		if (_putchar(c) == -1)
+		if (write(STDERR_FILENO, &c, 1) == -1)
 		{
 			close(fd);
 			return (0);
 		}
 		total_bytes_read++;
 		letters--;
+		if (total_bytes_read >= 100) {
+			break;  // Stop after printing the first 100 characters
+		}
 	}
+
+	// Restore STDOUT
+	if (dup2(original_stdout, STDOUT_FILENO) == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	close(original_stdout);
+
 	close(fd);
 	return (total_bytes_read);
 }
