deb0a1cbb5ec80778d5c3f224300e05caf4c6dc0
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-22 22:07:54 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     }
 
     current = *h;
-    for (i = 0; i < idx - 1 && current != NULL; i++)
+    for (i = 0; i < idx && current != NULL; i++)
         current = current->next;
 
     if (current == NULL)
@@ -46,13 +46,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     }
 
     /* Insert in the middle or at the end */
-    new_node->next = current->next;
-    new_node->prev = current;
+    new_node->next = current;
+    new_node->prev = current->prev;
 
-    if (current->next != NULL)
-        current->next->prev = new_node;
+    if (current->prev != NULL)
+        current->prev->next = new_node;
+    else
+        *h = new_node;
 
-    current->next = new_node;
+    current->prev = new_node;
 
     return new_node;
 }
