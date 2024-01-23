6c063d17c69524090d20fb1a3704f266441f948a
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-22 21:02:28 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
     dlistint_t *new, *current;
     unsigned int i;
+	int divisor = 1;
 
     if (h == NULL)
         return (NULL);
@@ -58,7 +59,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     _putchar(' ');
 
     /* Print each digit individually without recursion */
-    int divisor = 1;
+ 
     while (n / divisor >= 10)
         divisor *= 10;
 
