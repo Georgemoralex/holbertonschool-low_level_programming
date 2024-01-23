2171f7b304f0663c4bea1e2e8a4bbf4d0588d52b
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-22 22:19:54 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,33 +12,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     dlistint_t *new_node, *current;
     unsigned int i;
 
-    if (h == NULL)
+    if (!h)
         return NULL;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
+    new_node = calloc(1, sizeof(dlistint_t));
+    if (!new_node)
         return NULL;
 
     new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = NULL;
 
     if (idx == 0)
     {
         /* Insert at the beginning */
         new_node->next = *h;
-        if (*h != NULL)
+        if (*h)
             (*h)->prev = new_node;
         *h = new_node;
-
         return new_node;
     }
 
     current = *h;
-    for (i = 0; i < idx - 1 && current != NULL; i++)
+    for (i = 0; i < idx - 1 && current; i++)
         current = current->next;
 
-    if (current == NULL)
+    if (!current)
     {
         /* Cannot insert beyond the end of the list */
         free(new_node);
@@ -49,12 +46,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     new_node->next = current->next;
     new_node->prev = current;
 
-    if (current->next != NULL)
+    if (current->next)
         current->next->prev = new_node;
     current->next = new_node;
 
     return new_node;
 }
+
 
 
 
