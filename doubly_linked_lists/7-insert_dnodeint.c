500a53810683dd9a5472153dfb0ff1246650992e
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-22 21:13:08 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,9 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at
- * a given position
- *
+ * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: head of the list
  * @idx: index of the new node
  * @n: value of the new node
@@ -46,6 +44,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
         current->next->prev = new;
 
     current->next = new;
+
+    /* Print the arrow -> as part of the output using putchar */
+    _putchar('-');
+    _putchar('>');
+    _putchar(' ');
+
+    /* Print each digit individually without recursion */
+    int divisor = 1;
+    while (n / divisor >= 10)
+        divisor *= 10;
+
+    while (divisor != 0)
+    {
+        _putchar(n / divisor + '0');
+        n %= divisor;
+        divisor /= 10;
+    }
+
+    _putchar('\n');
 
     return new;
 }
