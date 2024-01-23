09b446c6348881317212fb8fb4934063e886ff61
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-22 22:46:47 -0600

    s

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,29 +9,29 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;  // Use size_t for count
+    size_t count = 0; 
 
     if (h == NULL)
         return (count);
 
-    // Print arrow and count before the actual elements
+    
     _putchar('-');
     _putchar('>');
     _putchar(' ');
     while (h->prev != NULL)
         h = h->prev;
 
-    // Iterate through the list to print elements
+
     while (h != NULL)
     {
         int num = h->n;
         int divisor = 1;
 
-        // Print arrow before each element
+  
         _putchar('-');
         _putchar('>');
 
-        // Print the actual element value
+
         while (num / divisor >= 10)
             divisor *= 10;
 
@@ -47,7 +47,7 @@ size_t print_dlistint(const dlistint_t *h)
         h = h->next;
     }
 
-    // Print the count after printing all elements
+
     _putchar('-');
     _putchar('>');
     _putchar(' ');
