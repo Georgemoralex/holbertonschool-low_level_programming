f8eda2c3b23abd48b34a5527d80544dbf3ee9ea8
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-22 21:25:21 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     dlistint_t *new, *current;
     unsigned int i;
 	int divisor = 1;
-
+	
     if (h == NULL)
         return NULL;
 
