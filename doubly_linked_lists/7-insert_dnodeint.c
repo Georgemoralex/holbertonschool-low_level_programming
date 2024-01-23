f6c0f5395660943cb9d0b9d302a26a94101f6a84
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-22 21:09:57 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,44 +11,43 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
-	dlistint_t *head;
-	unsigned int i;
+    dlistint_t *new, *head;
+    unsigned int i;
 
-	new = NULL;
-	if (idx == 0)
-		new = add_dnodeint(h, n);
-	else
-	{
-		head = *h;
-		i = 1;
-		if (head != NULL)
-			while (head->prev != NULL)
-				head = head->prev;
-		while (head != NULL)
-		{
-			if (i == idx)
-			{
-				if (head->next == NULL)
-					new = add_dnodeint_end(h, n);
-				else
-				{
-					new = malloc(sizeof(dlistint_t));
-					if (new != NULL)
-					{
-						new->n = n;
-						new->next = head->next;
-						new->prev = head;
-						head->next->prev = new;
-						head->next = new;
-					}
-				}
-				break;
-			}
-			head = head->next;
-			i++;
-		}
-	}
+    new = NULL;
+    if (idx == 0)
+    {
+        new = add_dnodeint(h, n);
+    }
+    else
+    {
+        head = *h;
+        i = 1;
 
-	return (new);
+        // Traverse the list to the position before the desired index
+        while (head != NULL && i < idx)
+        {
+            head = head->next;
+            i++;
+        }
+
+        // Check if the index is within the current size of the list
+        if (head != NULL)
+        {
+            new = malloc(sizeof(dlistint_t));
+            if (new != NULL)
+            {
+                new->n = n;
+                new->prev = head;
+                new->next = head->next;
+
+                if (head->next != NULL)
+                    head->next->prev = new;
+
+                head->next = new;
+            }
+        }
+    }
+
+    return new;
 }
