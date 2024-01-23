d9407b54755d63374678826535322a23a4235539
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-22 19:15:38 -0600

    s

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,35 +1,36 @@
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char c;
-	ssize_t total_bytes_read = 0;
 	int fd;
-	
-	if (filename == NULL)
-	{
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
 		return (0);
-	}
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		return (0);
-	}
 
-	while (letters > 0 && read(fd, &c, 1) == 1)
-	{
-		if (write(STDERR_FILENO, &c, 1) == -1)
-		{
-			close(fd);
-			return (0);
-		}
-		total_bytes_read++;
-		letters--;
-		if (total_bytes_read >= 100) {
-			break;
-		}
-	}
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
-	return (total_bytes_read);
+
+	free(buf);
+
+	return (nwr);
 }
