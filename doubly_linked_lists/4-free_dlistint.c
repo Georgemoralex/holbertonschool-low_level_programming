95ce4cf0bea39f52a68f20956279aada865c6280
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-24 23:06:24 -0600

    reload

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -2,10 +2,10 @@
 
 /**
  * free_dlistint - frees a dlistint_t list
+ *
  * @head: head of the list
  * Return: no return
  */
-
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
  * @h: head of the list
  * @idx: index of the new node
  * @n: value of the new node
@@ -9,49 +11,48 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *current;
-    unsigned int i;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-    if (!h)
-        return NULL;
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
 
-    new_node = calloc(1, sizeof(dlistint_t));
-    if (!new_node)
-        return NULL;
-
-    new_node->n = n;
-
-    if (idx == 0)
-    {
-        /* Insert at the beginning */
-        new_node->next = *h;
-        if (*h)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return new_node;
-    }
-
-    current = *h;
-    for (i = 0; i < idx - 1 && current; i++)
-        current = current->next;
-
-    if (!current)
-    {
-        /* Cannot insert beyond the end of the list */
-        free(new_node);
-        return NULL;
-    }
-
-    /* Insert in the middle or at the end */
-    new_node->next = current->next;
-    new_node->prev = current;
-
-    if (current->next)
-        current->next->prev = new_node;
-    current->next = new_node;
-
-    return new_node;
+	return (new);
 }
+
 
 
 
