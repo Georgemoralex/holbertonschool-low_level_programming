6aa618c338788b14ad132ac7f8544bf87d8fb5a9
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-01-24 22:58:50 -0600

    s

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,46 +11,36 @@ size_t print_dlistint(const dlistint_t *h)
 	int count;
 
 	count = 0;
-
 	if (h == NULL)
 		return (count);
-
 	while (h->prev != NULL)
 		h = h->prev;
-
 	while (h != NULL)
 	{
 		int num = h->n;
-
 		char str[12];
 		int i = 0;
-
 		if (num < 0)
 		{
 			_putchar('-');
 			num = -num;
 		}
-
 		while (num != 0)
 		{
 			str[i++] = num % 10 + '0';
 			num /= 10;
 		}
-
 		if (i == 0)
 		{
 			_putchar('0');
 		}
-
 		for (i--; i >= 0; i--)
 		{
 			_putchar(str[i]);
 		}
-
 		_putchar('\n');
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
