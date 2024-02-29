07878b3e984cb33f679232616e89f14eaf5075dc
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-02-29 02:36:05 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
     dlistint_t *head = NULL;
     dlistint_t *node;
