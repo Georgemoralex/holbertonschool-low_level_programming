ad2bfaf0647f75ef28831cd5290cfa9f74d67e14
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-02-29 02:55:17 -0600

    s

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,26 +1,47 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	int count;
 
-    if (h == NULL)
-        return (count);
+	count = 0;
+	if (h == NULL)
+		return (count);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		int num = h->n;
+		char str[12];
+		int i = 0;
 
-    printf("-> %d\n", h->n);
-    count++;
-
-    h = h->next;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        count++;
-        h = h->next;
-    }
-
-    printf("-> %lu elements\n", count);
-
-    return (count);
+		if (num < 0)
+		{
+			_putchar('-');
+			num = -num;
+		}
+		while (num != 0)
+		{
+			str[i++] = num % 10 + '0';
+			num /= 10;
+		}
+		if (i == 0)
+		{
+			_putchar('0');
+		}
+		for (i--; i >= 0; i--)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
