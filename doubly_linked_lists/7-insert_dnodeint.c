7134fc626ca19bafbef8ff79a8a1073d487bfc68
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-02-29 02:32:33 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -7,18 +7,15 @@ int main(void)
     dlistint_t *head = NULL;
     dlistint_t *node;
 
-    // Inserting elements dynamically
     head = add_dnodeint_end(&head, 98);
     head = add_dnodeint_end(&head, 9);
     head = add_dnodeint_end(&head, 6);
 
-    // Printing the elements
     printf("-> %d\n", head->n); // Print the first element separately
     for (node = head->next; node != NULL; node = node->next)
         printf("%d\n", node->n);
     printf("-> 3 elements\n");
 
-    // Reversing and printing the elements
     printf("-> %d\n", node->prev->n); // Print the last element separately
     for (; node != NULL; node = node->prev)
         printf("%d\n", node->n);
