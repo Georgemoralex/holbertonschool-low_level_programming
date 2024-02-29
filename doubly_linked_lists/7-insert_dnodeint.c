d70ba2c9cad7760a949b7f7645d4f05f9ea2efba
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-02-29 02:31:19 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,55 +1,28 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * insert_dnodeint_at_index - inserts a new node at
- * a given position
- *
- * @h: head of the list
- * @idx: index of the new node
- * @n: value of the new node
- * Return: the address of the new node, or NULL if it failed
- */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+int main(void)
 {
-    dlistint_t *new;
-    dlistint_t *head;
-    unsigned int i;
+    dlistint_t *head = NULL;
+    dlistint_t *node;
 
-    new = NULL;
-    if (idx == 0)
-        new = add_dnodeint(h, n);
-    else
-    {
-        head = *h;
-        i = 1;
-        if (head != NULL)
-            while (head->prev != NULL)
-                head = head->prev;
-        while (head != NULL)
-        {
-            if (i == idx)
-            {
-                if (head->next == NULL)
-                    new = add_dnodeint_end(h, n);
-                else
-                {
-                    new = malloc(sizeof(dlistint_t));
-                    if (new != NULL)
-                    {
-                        new->n = n;
-                        new->next = head->next;
-                        new->prev = head;
-                        head->next->prev = new;
-                        head->next = new;
-                    }
-                }
-                break;
-            }
-            head = head->next;
-            i++;
-        }
-    }
+    // Inserting elements dynamically
+    head = add_dnodeint_end(&head, 98);
+    head = add_dnodeint_end(&head, 9);
+    head = add_dnodeint_end(&head, 6);
 
-    return (new);
+    // Printing the elements
+    printf("-> %d\n", head->n); // Print the first element separately
+    for (node = head->next; node != NULL; node = node->next)
+        printf("%d\n", node->n);
+    printf("-> 3 elements\n");
+
+    // Reversing and printing the elements
+    printf("-> %d\n", node->prev->n); // Print the last element separately
+    for (; node != NULL; node = node->prev)
+        printf("%d\n", node->n);
+    printf("-> 3 elements\n");
+
+    return (0);
 }
