220c4c7d4d83be2527841b5274ea53c30315c321
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-02-29 02:39:14 -0600

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,25 +1,50 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the head of the linked list
+ * @idx: index where the new node should be inserted
+ * @n: value to be stored in the new node
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *head = NULL;
-    dlistint_t *node;
+    dlistint_t *new_node, *temp = *h;
+    unsigned int count = 0;
 
-    head = add_dnodeint_end(&head, 98);
-    head = add_dnodeint_end(&head, 9);
-    head = add_dnodeint_end(&head, 6);
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-    printf("-> %d\n", head->n);
-    for (node = head->next; node != NULL; node = node->next)
-        printf("%d\n", node->n);
-    printf("-> 3 elements\n");
+    new_node->n = n;
 
-    printf("-> %d\n", node->prev->n);
-    for (; node != NULL; node = node->prev)
-        printf("%d\n", node->n);
-    printf("-> 3 elements\n");
+    if (idx == 0)
+    {
+        new_node->prev = NULL;
+        new_node->next = *h;
+        if (*h != NULL)
+            (*h)->prev = new_node;
+        *h = new_node;
+        return (new_node);
+    }
 
-    return (0);
+    while (temp != NULL)
+    {
+        if (count == idx - 1)
+        {
+            new_node->next = temp->next;
+            new_node->prev = temp;
+            if (temp->next != NULL)
+                temp->next->prev = new_node;
+            temp->next = new_node;
+            return (new_node);
+        }
+        count++;
+        temp = temp->next;
+    }
+
+    free(new_node);
+    return (NULL);
 }
