b0ce6a215912864be9487c1b558b2106e1dc21b6
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-02-29 02:44:21 -0600

    s

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,40 +8,25 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+    size_t count = 0;
 
-	count = 0;
-	if (h == NULL)
-		return (count);
-	while (h->prev != NULL)
-		h = h->prev;
-	while (h != NULL)
-	{
-		int num = h->n;
-		char str[12];
-		int i = 0;
+    if (h == NULL)
+        return (count);
 
-		if (num < 0)
-		{
-			_putchar('-');
-			num = -num;
-		}
-		while (num != 0)
-		{
-			str[i++] = num % 10 + '0';
-			num /= 10;
-		}
-		if (i == 0)
-		{
-			_putchar('0');
-		}
-		for (i--; i >= 0; i--)
-		{
-			_putchar(str[i]);
-		}
-		_putchar('\n');
-		count++;
-		h = h->next;
-	}
-	return (count);
+    // Print the arrow before the first element
+    printf("-> %d\n", h->n);
+    count++;
+
+    // Move to the next node
+    h = h->next;
+
+    // Print the rest of the elements
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        count++;
+        h = h->next;
+    }
+
+    return (count);
 }
---
+++ doubly_linked_lists/0-print_dlistint2.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+	if (h == NULL)
+		return (count);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		int num = h->n;
+		char str[12];
+		int i = 0;
+
+		if (num < 0)
+		{
+			_putchar('-');
+			num = -num;
+		}
+		while (num != 0)
+		{
+			str[i++] = num % 10 + '0';
+			num /= 10;
+		}
+		if (i == 0)
+		{
+			_putchar('0');
+		}
+		for (i--; i >= 0; i--)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
