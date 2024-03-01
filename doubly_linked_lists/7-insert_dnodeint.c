df8f556505228bbf10a32e3923cb60c3dee648d6
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-01 14:34:07 -0600

    betty style kennel

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,40 +11,40 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *temp = *h;
-    unsigned int count = 0;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int count = 0;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
+	new_node->n = n;
 
-    if (idx == 0)
-    {
-        new_node->prev = NULL;
-        new_node->next = *h;
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return (new_node);
-    }
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 
-    while (temp != NULL)
-    {
-        if (count == idx - 1)
-        {
-            new_node->next = temp->next;
-            new_node->prev = temp;
-            if (temp->next != NULL)
-                temp->next->prev = new_node;
-            temp->next = new_node;
-            return (new_node);
-        }
-        count++;
-        temp = temp->next;
-    }
+	while (temp != NULL)
+	{
+		if (count == idx - 1)
+		{
+			new_node->next = temp->next;
+			new_node->prev = temp;
+			if (temp->next != NULL)
+				temp->next->prev = new_node;
+			temp->next = new_node;
+			return (new_node);
+		}
+		count++;
+		temp = temp->next;
+	}
 
-    free(new_node);
-    return (NULL);
+	free(new_node);
+	return (NULL);
 }
